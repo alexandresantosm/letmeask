331638de269a63233a3eef451b965f1bd3846c87
Alexandre Menezes <amenezes031@gmail.com>
2021-06-22 16:39:47 -0300

    add properties to the components

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-  
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button text="Botão 1" /> {/* texto */}
+      <Button text={1} /> {/* numero */}
+      <Button text={["1", "2", "3"]} /> {/* array */}
+      <Button>Children</Button> {/* conteudo */}
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,8 @@
-export function Button() {
-  return (
-    <button>Clique aqui!</button>
-  );
+type ButtonProps = {
+  text?: string | number | Array<string>;
+  children?: string;
+};
+
+export function Button({ text, children }: ButtonProps) {
+  return <button>{text || "Clique aqui!" || children}</button>;
 }
