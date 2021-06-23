acd94831c93c957c40a435eb78919156a73f919c
Alexandre Menezes <amenezes031@gmail.com>
2021-06-23 15:02:51 -0300

    add component Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "./components/Button";
+// import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button />
+      <Home />
     </div>
   );
 }
