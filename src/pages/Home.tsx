23a4775ac0571def52da091de9b9070ecf4b8a2b
Alexandre Menezes <amenezes031@gmail.com>
2021-06-23 16:23:43 -0300

    page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,13 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import { Button } from "../components/Button";
+
+import "../styles/pages/auth.scss";
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img
           src={illustrationImg}
@@ -17,20 +21,22 @@ export function Home() {
       </aside>
 
       <main>
-        <img src={logoImg} alt="Letmeask" />
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
 
-        <button>
-          <img src={googleIconImg} alt="Logo da Google" />
-          Crie sua sala com o Google
-        </button>
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo da Google" />
+            Crie sua sala com o Google
+          </button>
 
-        <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
 
-        <form>
-          <input type="text" placeholder="Digite o código da sala" />
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
 
-          <button type="submit">Entrar na sala</button>
-        </form>
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
       </main>
     </div>
   );
