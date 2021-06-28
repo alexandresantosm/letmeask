43984359fde235aa0a86d69b6ccb069abc7fe220
Alexandre Menezes <amenezes031@gmail.com>
2021-06-28 11:26:16 -0300

    add flow to enter the room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { Button } from "../components/inputs/Button";
@@ -10,12 +11,14 @@ import { MainContent } from "../components/MainContent";
 import { CreateRoomButton } from "../components/CreateRoomButton";
 
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/pages/auth.scss";
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function navigateToNewRoom() {
     if (!user) {
@@ -23,6 +26,23 @@ export function Home() {
     }
 
     history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -36,8 +56,13 @@ export function Home() {
 
           <Separator />
 
-          <form>
-            <TextInput type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <TextInput
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
 
             <Button type="submit">Entrar na sala</Button>
           </form>
